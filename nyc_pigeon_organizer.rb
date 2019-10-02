0118b7f1353095013ec20c71c340276d985652f0
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-10-02 18:30:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,10 +19,11 @@ if !pigeon_data_by_name[names][key]
   pigeon_data_by_name[names]=[]
 end
 
- 
+pigeon_data_by_name[names][key] << another_key.to_s
 end
 end
 end
+
  binding.pry
 pigeon_data_by_name 
 
