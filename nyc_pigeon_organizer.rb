f137f92ff6fd56a3f6e26acc6f64a8cfb7d615ed
Carlie Anglemire <carlie.anglemire@gmail.com>
2019-10-02 18:02:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,9 +5,11 @@ pigeon_data_by_name = {}
 
 pigeon_data.each do |key, hash_value|
  
-
+  
 hash_value.each do |another_key, another_hash_value|
+ 
 another_hash_value.each do |names|
+
  
 if !pigeon_data_by_name[names]
   pigeon_data_by_name[names] = {}
@@ -15,22 +17,22 @@ if !pigeon_data_by_name[names]
 end
 
 if !pigeon_data_by_name[names][key] = [
-  another_key.to_s]
-end
+    another_key.to_s]
 
-#if pigeon_data_by_name[names][key]
-#if ![key]
-#pigeon_data_by_name[names][key]+=[another_key.to_s]
+end 
 
+if pigeon_data_by_name[names][key]
+ pigeon_data_by_name[names][:color] = []
+  
 
+  
  
-end
 end
 end
 end
   
 end
-binding.pry
+ binding.pry
 pigeon_data_by_name 
 
 end
